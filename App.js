cef81596d8b12af7a715d267be1ef2864ddb8734
Diego FINOCCHIARO <diegofino15@gmail.com>
2023-10-05 18:12:56 +0200

    Updated theme setup

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { PaperProvider, useTheme } from 'react-native-paper';
 import useState from 'react-usestateref'
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-import { useFonts } from './hooks/useFonts';
+import { useFonts, setThemeData } from './hooks/useFonts';
 import { AuthStack } from './views/AuthStack';
 import { AppStack } from './views/AppStack/AppStack';
 import { AppContextProvider } from './utils/AppContext';
@@ -30,101 +30,7 @@ function App() {
 
   // App theme
   const theme = useTheme();
-  theme.colors = {
-    background: '#FFF',
-    onBackground: '#000',
-
-    surface: '#ECECEC',
-    onSurface: '#000',
-    onSurfaceDisabled: '#888',
-
-    primary: '#1985A1',
-    onPrimary: '#FFF',
-    onPrimaryDisabled: '#888',
-  };
-  theme.fonts = {
-    default: {
-      fontSize: 17,
-      fontWeight: 'normal',
-      fontFamily: 'Montserrat-Medium',
-    },
-
-    titleLarge: {
-      fontSize: 35.0,
-      fontWeight: 'bold',
-      fontFamily: 'Montserrat-Medium',
-      color: theme.colors.onBackground,
-    },
-    titleMedium: {
-      fontSize: 25.0,
-      fontWeight: 'bold',
-      fontFamily: 'Montserrat-Medium',
-      color: theme.colors.onBackground,
-    },
-    titleSmall: {
-      fontSize: 20.0,
-      fontWeight: 'bold',
-      fontFamily: 'Montserrat-Medium',
-      color: theme.colors.onBackground,
-    },
-
-    headlineLarge: {
-      fontSize: 35.0,
-      fontWeight: 'bold',
-      fontFamily: 'Bitter-Bold',
-      color: theme.colors.onBackground,
-    },
-    headlineMedium: {
-      fontSize: 17.0,
-      fontWeight: 'bold',
-      fontFamily: 'Bitter-Medium',
-      color: theme.colors.onBackground,
-    },
-    headlineSmall: {
-      fontSize: 12.0,
-      fontWeight: 'bold',
-      fontFamily: 'Bitter-Medium',
-      color: theme.colors.onBackground,
-    },
-
-    bodyLarge: {
-      fontSize: 17.0,
-      fontWeight: 'normal',
-      fontFamily: 'Montserrat-Medium',
-      color: theme.colors.onSurface,
-    },
-    bodyMedium: {
-      fontSize: 15.0,
-      fontWeight: 'normal',
-      fontFamily: 'Montserrat-Medium',
-      color: theme.colors.onSurface,
-    },
-    bodySmall: {
-      fontSize: 13.0,
-      fontWeight: 'normal',
-      fontFamily: 'Montserrat-Medium',
-      color: theme.colors.onSurface,
-    },
-
-    labelLarge: {
-      fontSize: 17.0,
-      fontWeight: 'normal',
-      fontFamily: 'Montserrat-Regular',
-      color: theme.colors.onSurfaceDisabled,
-    },
-    labelMedium: {
-      fontSize: 15.0,
-      fontWeight: 'normal',
-      fontFamily: 'Montserrat-Regular',
-      color: theme.colors.onSurfaceDisabled,
-    },
-    labelSmall: {
-      fontSize: 13.0,
-      fontWeight: 'normal',
-      fontFamily: 'Montserrat-Regular',
-      color: theme.colors.onSurfaceDisabled,
-    },
-  };
+  setThemeData(theme);
 
   // Fonts
   const [_fontsLoaded, setFontsLoaded, fontsLoadedRef] = useState(false);
--- hooks/useFonts.js
+++ hooks/useFonts.js
@@ -1,14 +1,115 @@
 import { loadAsync } from 'expo-font';
+import { useTheme } from 'react-native-paper';
 
-const useFonts = async () =>
-  await loadAsync({
-    'Montserrat-Regular': require('../assets/fonts/Montserrat-Regular.ttf'),
-    'Montserrat-Medium': require('../assets/fonts/Montserrat-Medium.ttf'),
-    'Montserrat-Bold': require('../assets/fonts/Montserrat-Bold.ttf'),
+const useFonts = async () => await loadAsync({
+  'Montserrat-Regular': require('../assets/fonts/Montserrat-Regular.ttf'),
+  'Montserrat-Medium': require('../assets/fonts/Montserrat-Medium.ttf'),
+  'Montserrat-Bold': require('../assets/fonts/Montserrat-Bold.ttf'),
 
-    'Bitter-Regular': require('../assets/fonts/Bitter-Regular.ttf'),
-    'Bitter-Medium': require('../assets/fonts/Bitter-Medium.ttf'),
-    'Bitter-Bold': require('../assets/fonts/Bitter-Bold.ttf'),
-  });
+  'Bitter-Regular': require('../assets/fonts/Bitter-Regular.ttf'),
+  'Bitter-Medium': require('../assets/fonts/Bitter-Medium.ttf'),
+  'Bitter-Bold': require('../assets/fonts/Bitter-Bold.ttf'),
+});
 
-export { useFonts };
+
+function setThemeData(theme) {
+  theme.colors = {
+    background: '#FFF',
+    onBackground: '#000',
+  
+    surface: '#ECECEC',
+    onSurface: '#000',
+    onSurfaceDisabled: '#888',
+  
+    primary: '#1985A1',
+    onPrimary: '#FFF',
+    onPrimaryDisabled: '#888',
+  };
+  theme.fonts = {
+    default: {
+      fontSize: 17,
+      fontWeight: 'normal',
+      fontFamily: 'Montserrat-Medium',
+    },
+  
+    titleLarge: {
+      fontSize: 35.0,
+      fontWeight: 'bold',
+      fontFamily: 'Montserrat-Medium',
+      color: theme.colors.onBackground,
+    },
+    titleMedium: {
+      fontSize: 25.0,
+      fontWeight: 'bold',
+      fontFamily: 'Montserrat-Medium',
+      color: theme.colors.onBackground,
+    },
+    titleSmall: {
+      fontSize: 20.0,
+      fontWeight: 'bold',
+      fontFamily: 'Montserrat-Medium',
+      color: theme.colors.onBackground,
+    },
+  
+    headlineLarge: {
+      fontSize: 35.0,
+      fontWeight: 'bold',
+      fontFamily: 'Bitter-Bold',
+      color: theme.colors.onBackground,
+    },
+    headlineMedium: {
+      fontSize: 17.0,
+      fontWeight: 'bold',
+      fontFamily: 'Bitter-Medium',
+      color: theme.colors.onBackground,
+    },
+    headlineSmall: {
+      fontSize: 12.0,
+      fontWeight: 'bold',
+      fontFamily: 'Bitter-Medium',
+      color: theme.colors.onBackground,
+    },
+  
+    bodyLarge: {
+      fontSize: 17.0,
+      fontWeight: 'normal',
+      fontFamily: 'Montserrat-Medium',
+      color: theme.colors.onSurface,
+    },
+    bodyMedium: {
+      fontSize: 15.0,
+      fontWeight: 'normal',
+      fontFamily: 'Montserrat-Medium',
+      color: theme.colors.onSurface,
+    },
+    bodySmall: {
+      fontSize: 13.0,
+      fontWeight: 'normal',
+      fontFamily: 'Montserrat-Medium',
+      color: theme.colors.onSurface,
+    },
+  
+    labelLarge: {
+      fontSize: 17.0,
+      fontWeight: 'normal',
+      fontFamily: 'Montserrat-Regular',
+      color: theme.colors.onSurfaceDisabled,
+    },
+    labelMedium: {
+      fontSize: 15.0,
+      fontWeight: 'normal',
+      fontFamily: 'Montserrat-Regular',
+      color: theme.colors.onSurfaceDisabled,
+    },
+    labelSmall: {
+      fontSize: 13.0,
+      fontWeight: 'normal',
+      fontFamily: 'Montserrat-Regular',
+      color: theme.colors.onSurfaceDisabled,
+    },
+  };
+}
+
+
+
+export { useFonts, setThemeData };
